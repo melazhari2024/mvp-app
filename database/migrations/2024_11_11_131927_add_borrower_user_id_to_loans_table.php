d6080b2e132ba70304aeec1e022a1bc02768de03
Mohamed <melazhari@prologuetechnology.com>
2024-11-12 11:19:45 -0500

    Adding the borrower user id to loans table

---
+++ database/migrations/2024_11_11_131927_add_borrower_user_id_to_loans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->unsignedBigInteger('borrower_user_id')->after('duration');
+            $table->foreign('borrower_user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->dropForeign(['borrower_user_id']);
+            $table->dropColumn('borrower_user_id');
+        });
+    }
+};
